9d444b1446cec8beb1c665db03b7f9a6c6b0869f
iqbalrevvin <iqbal.revvin@gmail.com>
2019-07-28 21:53:05 +0700

    Update payroll.php

--- application/config/payroll.php
+++ application/config/payroll.php
@@ -2,11 +2,10 @@
 
 $config['staffattendance'] = array(
     'present' => 1,
-    'half_day' => 2,
-    'late' => 3,
-    'absent' => 4,
-    'holiday' => 5,
-    'sakit' => 6,
+    'half_day' => 4,
+    'late' => 2,
+    'absent' => 3,
+    'holiday' => 5
 );
 
 $config['contracttype'] = array(
